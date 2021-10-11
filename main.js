955dcf0b6853701c46ed55f1f96b97f9314ba72e
Roberto Paz <fsjedi@outlook.com>
2021-10-11 10:28:48 -0400

    final

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-let view = document.getElementById('view-button');
-let close = document.getElementById('close-button');
-let codey = document.getElementById('codey');
+let view = document.getElementById('view-button')
+let close = document.getElementById('close-button')
+let codey = document.getElementById('codey')
 
-let open = function() {
-  codey.style.display = 'block';
-  close.style.display = 'block';
-};
+let open = function () {
+  codey.style.display = 'block'
+  close.style.display = 'block'
+}
 
-let hide = function() {
-  codey.style.display = 'none';
-  close.style.display = 'none';
-};
+let hide = function () {
+  codey.style.display = 'none'
+  close.style.display = 'none'
+}
 
-view.addEventListener('click', open);
-close.addEventListener('click', hide);
+view.addEventListener('click', open)
+close.addEventListener('click', hide)
 
 // Write your code here
+function textChange() {
+  view.innerHTML = 'Hello World!'
+}
+
+function textReturn() {
+  view.innerHTML = 'View'
+}
+
+view.addEventListener('click', textChange)
+close.addEventListener('click', textReturn)
